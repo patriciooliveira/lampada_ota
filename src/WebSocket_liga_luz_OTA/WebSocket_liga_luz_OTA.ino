6fd206027a48bea54ea60948ce3d0d74921dd892
Patricio Oliveira <apenaspatricio@gmail.com>
2017-03-28 20:31:52 -0300

    ...
    ...

--- src/WebSocket_liga_luz_OTA/WebSocket_liga_luz_OTA.ino
+++ src/WebSocket_liga_luz_OTA/WebSocket_liga_luz_OTA.ino
@@ -70,7 +70,7 @@ void setup() {
   ArduinoOTA.setPort(8266);
 
   // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname("lampada001");
+  ArduinoOTA.setHostname(serial);
 
   // No authentication by default  ArduinoOTA.setPassword((const char *)"123");
   //ArduinoOTA.setPassword();
